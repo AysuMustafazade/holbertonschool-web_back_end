ad2b83f2c3c3d2c7be80e32a5fddc31c87b1e0ae
AysuMustafazade <mustafazadays16@gmail.com>
2026-05-26 19:50:37 +0400

    Python

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> Generator[float, None, None]:
     """Async Generator function that takes no arguments"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
