06eb299ca2ea3ae2885c4c9da68d5f6c9cea7b68
AysuMustafazade <mustafazadays16@gmail.com>
2026-05-26 19:52:44 +0400

    Python

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -5,6 +5,7 @@ from typing import List
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     start = time.perf_counter()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
