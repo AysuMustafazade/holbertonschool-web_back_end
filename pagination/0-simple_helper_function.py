2888d98997d26c31f0bb8029d70ea10f52470031
AysuMustafazade <mustafazadays16@gmail.com>
2026-06-13 15:59:32 +0400

    pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def index_range(page, page_size):
     """Returns a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for those
