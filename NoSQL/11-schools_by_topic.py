2ac9b8757ce679bd883688970314cf725ce548b2
AysuMustafazade <mustafazadays16@gmail.com>
2026-06-21 21:58:28 +0400

    NoSQL

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""returns the list of school having a specific topic"""
+def schools_by_topics(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    return list(mongo_collection.find({"topics": topic}))
